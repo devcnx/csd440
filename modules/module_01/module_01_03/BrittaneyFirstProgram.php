6a04cd12af1ff3ed204f7ec6aa98b0822afa012b
Brittaney Perry-Morgan <brittaney@devcnx.com>
2026-04-05 18:41:07 -0500

    chore(docs): fix initial docstring in module 1 programming assignment

--- modules/module_01/module_01_03/BrittaneyFirstProgram.php
+++ modules/module_01/module_01_03/BrittaneyFirstProgram.php
@@ -13,7 +13,7 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
+<head>66    
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Brittaney's First PHP Program</title>
